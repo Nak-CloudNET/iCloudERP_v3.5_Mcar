30039a7cc2832589936a80093092c0064c9a0624
Thintha.Chan <thintha.chan07@gmail.com>
2018-08-29 15:48:25 +0700

    Thintha Fixed page footer a5.

--- themes/default/views/sales/invoice_st_a4.php
+++ themes/default/views/sales/invoice_st_a4.php
@@ -528,9 +528,6 @@
 
         </div>
 
-
-<!--jtjflsjdf-->
-
         <div style="width: 821px;margin: 20px">
             <a class="btn btn-warning no-print" href="<?= site_url('sales'); ?>" style="border-radius: 0">
                 <i class="fa fa-hand-o-left" aria-hidden="true"></i>&nbsp;<?= lang("back"); ?>
